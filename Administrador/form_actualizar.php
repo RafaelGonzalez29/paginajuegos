93a981cf124fc08e6f4536e0f786301e58010441
RafaelGonzalez29 <150069891+RafaelGonzalez29@users.noreply.github.com>
2023-11-16 16:13:39 -0500

    Add files via upload

--- Administrador/form_actualizar.php
+++ Administrador/form_actualizar.php
@@ -1,6 +1,4 @@
-<?php
-include('../Administrador/modulosAdmin/header.php')
-?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -8,49 +6,170 @@ include('../Administrador/modulosAdmin/header.php')
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+  <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+
+
+    <link rel="stylesheet" href="../assets/css/adminlte.min.css">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">  
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css"> 
+
+    <link rel="stylesheet" href="../assets/css/estilos.admin.css">
+    
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">  
+
+    <link rel="stylesheet" href="../assets/css/sweetalert2.min.css">
+    <link rel="stylesheet" href="../assets/css/select2.min.css">    
+  
+
  
   
   <title>Document</title>
 </head>
 <body>
 
+<nav class="navbar navbar-expand-md estilos-nav">
+      <div class="container-fluid ">
+        <a href="index.php" class="navbar-brand">
+        <svg width="40" height="40" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M11.5 6.027C11.5 6.15961 11.4473 6.28679 11.3536 6.38055C11.2598 6.47432 11.1326 6.527 11 6.527C10.8674 6.527 10.7402 6.47432 10.6464 6.38055C10.5527 6.28679 10.5 6.15961 10.5 6.027C10.5 5.89439 10.5527 5.76722 10.6464 5.67345C10.7402 5.57968 10.8674 5.527 11 5.527C11.1326 5.527 11.2598 5.57968 11.3536 5.67345C11.4473 5.76722 11.5 5.89439 11.5 6.027ZM10 7.527C10.1326 7.527 10.2598 7.47432 10.3536 7.38055C10.4473 7.28679 10.5 7.15961 10.5 7.027C10.5 6.89439 10.4473 6.76722 10.3536 6.67345C10.2598 6.57968 10.1326 6.527 10 6.527C9.86739 6.527 9.74021 6.57968 9.64645 6.67345C9.55268 6.76722 9.5 6.89439 9.5 7.027C9.5 7.15961 9.55268 7.28679 9.64645 7.38055C9.74021 7.47432 9.86739 7.527 10 7.527ZM12.5 7.027C12.5 7.15961 12.4473 7.28679 12.3536 7.38055C12.2598 7.47432 12.1326 7.527 12 7.527C11.8674 7.527 11.7402 7.47432 11.6464 7.38055C11.5527 7.28679 11.5 7.15961 11.5 7.027C11.5 6.89439 11.5527 6.76722 11.6464 6.67345C11.7402 6.57968 11.8674 6.527 12 6.527C12.1326 6.527 12.2598 6.57968 12.3536 6.67345C12.4473 6.76722 12.5 6.89439 12.5 7.027ZM11 8.527C11.1326 8.527 11.2598 8.47432 11.3536 8.38055C11.4473 8.28679 11.5 8.15961 11.5 8.027C11.5 7.89439 11.4473 7.76722 11.3536 7.67345C11.2598 7.57968 11.1326 7.527 11 7.527C10.8674 7.527 10.7402 7.57968 10.6464 7.67345C10.5527 7.76722 10.5 7.89439 10.5 8.027C10.5 8.15961 10.5527 8.28679 10.6464 8.38055C10.7402 8.47432 10.8674 8.527 11 8.527ZM4.5 5.527H5.5V6.527H6.5V7.527H5.5V8.527H4.5V7.527H3.5V6.527H4.5V5.527Z" fill="#7EF4C3"/>
+          <path d="M3.051 3.26C3.03392 3.19651 3.02953 3.13027 3.03808 3.06507C3.04664 2.99988 3.06797 2.93701 3.10085 2.88007C3.13373 2.82314 3.17752 2.77324 3.22971 2.73325C3.2819 2.69325 3.34147 2.66395 3.405 2.647L5.337 2.129C5.40342 2.1113 5.47277 2.10749 5.54073 2.1178C5.60869 2.1281 5.67379 2.15231 5.73198 2.18891C5.79016 2.22551 5.84017 2.27371 5.87889 2.33051C5.9176 2.3873 5.94419 2.45147 5.957 2.519C6.612 2.44 7.307 2.402 8 2.402C8.72 2.402 9.443 2.443 10.12 2.528C10.1317 2.45945 10.1576 2.39408 10.1959 2.33609C10.2343 2.27809 10.2844 2.22874 10.3429 2.19119C10.4015 2.15364 10.4672 2.12872 10.5359 2.11801C10.6046 2.1073 10.6748 2.11104 10.742 2.129L12.674 2.647C12.7477 2.66674 12.8159 2.70309 12.8734 2.75326C12.9309 2.80344 12.9762 2.86611 13.0057 2.93648C13.0353 3.00685 13.0483 3.08304 13.0438 3.15923C13.0394 3.23541 13.0175 3.30956 12.98 3.376C13.12 3.466 13.246 3.566 13.353 3.673C13.761 4.081 14.133 4.723 14.448 5.445C14.768 6.178 15.047 7.036 15.253 7.911C15.459 8.786 15.593 9.691 15.617 10.517C15.641 11.333 15.558 12.119 15.289 12.727C15.1663 13 14.9606 13.2272 14.7011 13.3763C14.4416 13.5253 14.1416 13.5886 13.844 13.557C13.208 13.49 12.729 13.163 12.331 12.784C12.086 12.552 11.835 12.258 11.592 11.976C11.466 11.828 11.342 11.684 11.224 11.553C10.496 10.749 9.627 10.026 8 10.026C6.373 10.026 5.504 10.749 4.776 11.553C4.657 11.684 4.534 11.828 4.408 11.976C4.165 12.258 3.914 12.551 3.669 12.784C3.271 13.164 2.792 13.49 2.156 13.557C1.85841 13.5886 1.55845 13.5253 1.29895 13.3763C1.03945 13.2272 0.833671 13 0.710999 12.727C0.440999 12.119 0.358999 11.332 0.381999 10.517C0.405999 9.691 0.541999 8.787 0.746999 7.911C0.952999 7.036 1.233 6.178 1.552 5.445C1.867 4.723 2.239 4.081 2.646 3.673C2.77673 3.54478 2.92205 3.43234 3.079 3.338C3.06749 3.31249 3.05812 3.28707 3.051 3.26ZM5.087 3.672C4.21 3.857 3.618 4.115 3.354 4.38C3.078 4.656 2.767 5.163 2.469 5.845C2.15286 6.58659 1.90256 7.35455 1.721 8.14C1.52919 8.92891 1.41565 9.73481 1.382 10.546C1.36 11.301 1.444 11.914 1.625 12.322C1.66207 12.4015 1.72308 12.4673 1.79947 12.5104C1.87587 12.5534 1.96382 12.5715 2.051 12.562C2.378 12.528 2.661 12.363 2.98 12.06C3.192 11.858 3.38 11.637 3.595 11.386C3.728 11.23 3.871 11.063 4.035 10.882C4.861 9.969 5.978 9.027 8 9.027C10.022 9.027 11.139 9.969 11.965 10.882C12.129 11.063 12.272 11.23 12.405 11.386C12.619 11.637 12.808 11.858 13.02 12.06C13.338 12.363 13.621 12.528 13.949 12.563C14.0363 12.5724 14.1243 12.5542 14.2007 12.511C14.2771 12.4677 14.3381 12.4017 14.375 12.322C14.555 11.914 14.64 11.302 14.618 10.546C14.5843 9.73481 14.4708 8.92892 14.279 8.14C14.0974 7.35456 13.8471 6.5866 13.531 5.845C13.233 5.163 12.921 4.655 12.646 4.38C12.382 4.115 11.79 3.857 10.913 3.672C10.063 3.493 9.036 3.402 8 3.402C6.964 3.402 5.937 3.493 5.087 3.672Z" fill="#F33D96"/>
+        </svg>
+          <span class="titulo-nav-gameflip">Gameflip</span>
+        </a>
+
+        
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menu" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <i class="fa-solid fa-bars" style="color: #000000;"></i>
+        </button>
+
+    
+
+        <div class="collapse navbar-collapse " id="menu">
+
+        
+          <ul class="navbar-nav pull-right">
+            <li class="nav-item ">
+              <a class="nav-link activate estilos-navegacion-titulos marginpedidosyvideo" href="videojuegos.php">Videojuegos</a>
+            </li>
+            
+            <li class=" nav-item ">
+              <a class="nav-link activate estilos-navegacion-titulos marginpedidosyvideo" href="pedidos.php">Pedidos</a>
+            </li>
+          <div class="centrar-icon-login">
+            <li class="nav-item ">  
+              <div class="hovericono">          
+                <a class="nav-link activate estilos-navegacion-login" id="nav_iniciarsesion" href="#login.php">
+                  <i class="fa-solid fa-user "></i> Iniciar sesion
+                </a> 
+              </div> 
+            </li>
+            <div class="contenedor-separador">
+              <span class="estilos-navegacion-titulos" id="nav-separador">&nbsp;| &nbsp;</span>
+            </div>
+            <li class="nav-item">
+              <a class="nav-link activate estilos-navegacion-titulos" id="nav_registro" href="views/registro.php">Registrarse</a>
+            </li>
+            </div>
+            <li class="nav-item"  id="nav_usuario">
+              <div class="dropdown show">
+              <a class="nav-link dropdown-toggle activate estilos-navegacion-titulos" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <img id="avatar_nav" src='' width="30" height="30" class="redonda"> &nbsp;
+              <span id="usuario_nav"></span></a>
+                   <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+    
+                   <a class="dropdown-item " href="/paginajuegos/controller/cerrarsesion.php"><i class="bi bi-person-fill-x"></i></i> Cerrar sesion</a>
+
+              </div>
+
+            </li>
+            
+            
+          </ul>
+          
+        </div>
+      </div>
+
+    </nav>
+
+
+    <nav class="navbar navbar-expand-md estilos-nav2">
+      <div class="container-fluid ">
+        <ul class="navbar-nav me-auto">
+         
+            <li class="nav-item ">  
+              <a class="nav-link activate  estilos-navegacion-titulos" href="#tienda.php">Tienda</a>
+            </li>
+            
+            <li class="nav-item">
+              <a class="nav-link activate estilos-navegacion-titulos" href="#registro.php">Juegos</a>
+            </li>
+            
+            <li class="nav-item">
+              <a class="nav-link activate estilos-navegacion-titulos" href="#registro.php">PC</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link activate estilos-navegacion-titulos" href="#registro.php">Xbox</a>
+            </li>
+            
+            <li class="nav-item">
+              <a class="nav-link activate estilos-navegacion-titulos" href="#registro.php">Playstation</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link activate estilos-navegacion-titulos" href="#registro.php">Nintendo</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link activate estilos-navegacion-titulos" href="#registro.php">Software</a>
+            </li>
+
+
+        </ul>   
+      </div>
+    </nav>
+
 <div class="container margindelformulario" id="main" >
       <div class="row">
         <div class="col-md-12">
           <fieldset>
             <legend>Datos del juego</legend>
-            <form method="POST" action="acciones.php" enctype="multipart/form-data">
-              <div class="row">
-                  <div class="col-md-6">
+            <form method="POST"  id="form-registrar"  enctype="multipart/form-data">
+              <div class="row" >
+                  <div class="col-md-8">
                       <div class="form-group">
                           <label>Titulo</label>
-                          <input type="text" class="form-control" name="titulo" required>
+                          <input type="text" class="form-control" name="titulo" id="titulo" >
                       </div>
                   </div>
               </div>
               <div class="row">
-                  <div class="col-md-12">
+                  <div class="col-md-8">
                       <div class="form-group">
                           <label>Descripción</label>
-                          <textarea class="form-control" name="descripcion" id="" cols="3" required></textarea>
+                          <textarea class="form-control" name="descripcion" id="descripcion" id="" cols="3" ></textarea>
                       </div>
                   </div>
               </div>
               <div class="row">
-                  <div class="col-md-4">
+                  <div class="col-md-8">
                       <div class="form-group">
                           <label>Categorias</label>
-                          <select class="form-control" name="categoria_id" required>
-                            <option value="">--SELECCIONE--</option>
+                          <select class="form-control" name="id_categoria" id="id_categoria" required>
+                          <option hidden>Accion</option>
+                            
                           </select>
                       </div>
                   </div>
               </div>
               <div class="row">
-                  <div class="col-md-12">
+                  <div class="col-md-8">
                       <div class="form-group">
                           <label>Foto</label>
-                          <input type="file" class="form-control" name="foto" required>
+                          <input type="file" class="form-control" name="foto" id="foto">
                       </div>
                   </div>
               </div>
@@ -58,11 +177,11 @@ include('../Administrador/modulosAdmin/header.php')
                   <div class="col-md-3">
                       <div class="form-group">
                           <label>Precio</label>
-                          <input type="text" class="form-control" name="precio" placeholder="0.00" required>
+                          <input type="text" class="form-control" name="precio" id="precio" placeholder="0.00" >
                       </div>
                   </div>
               </div>
-              <button type="submit" class="btn btn-primary">Actualizar</button>
+              <button type="submit" id="btnregistrar" class="btn btn-primary">actualizar</button>
               <a href="index.php" class="btn btn-default">Cancelar</a>
             </form>
           </fieldset>
@@ -74,13 +193,28 @@ include('../Administrador/modulosAdmin/header.php')
     
   
 
-
+    
 <?php
 include('../views/modulos/footer.php');
 ?>
 
 <script src="../assets/js/jquery.min.js"></script>
 <script src="../assets/js/bootstrap.min.js"></script>
+<script src="./js/form_actualizar.js"></script>
+
+<script src="../html/dist/js/jquery.validate.min.js"></script>
+<script src="../html/dist/js/additional-methods.min.js"></script>
+
+<script src="../assets/js/select2.min.js"></script>
+
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+
+
+<script src="../html/plugins/datatables/jquery.dataTables.min.js"></script>
+
+<script src="./js/index.js"></script>
+
   
 </body>
 </html>
--- Administrador/index.php
+++ Administrador/index.php
@@ -26,6 +26,23 @@ include('../Administrador/modulosAdmin/header.php')
 <?php
 include('../views/modulos/footer.php');
 ?>
+<script src="../assets/js/jquery.min.js"></script>
+<script src="../assets/js/bootstrap.min.js"></script>
+
+<script src="../html/dist/js/jquery.validate.min.js"></script>
+<script src="../html/dist/js/additional-methods.min.js"></script> 
+
+
+<script src="../html/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="./js/videojuegos.js"></script>
+
+<script src="../assets/js/select2.min.js"></script>
+
+
+
+<script src="../html/plugins/toastr/toastr.min.js"></script>
+
+<script src="./js/index.js"></script>
   
 </body>
 </html>
